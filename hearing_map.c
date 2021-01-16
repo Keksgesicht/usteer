658d37d5c049b167321ac02e783d5650fdce9e63
Jan B <keksgesicht23@gmail.com>
2021-01-16 15:25:53 +0100

    hearing_map.c license

--- hearing_map.c
+++ hearing_map.c
@@ -1,3 +1,20 @@
+/*
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *   Copyright (C) 2021 Jan Braun <jan-kai@braun-bs.de>
+ *   Copyright (C) 2021 Nico Petermann <nico.petermann3@gmail.com>
+ */
 
 struct BeaconReport {
 	usteer_node* BSSID;
