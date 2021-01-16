3d3cebf952764d8c1adfd0a2e43df7759c7ce700
Nico <nico.petermann3@gmail.com>
2021-01-16 15:19:53 +0100

    BP-11| Changed channel checks

--- hearing_map.c
+++ hearing_map.c
@@ -25,48 +25,24 @@ int getChannelFromFreq(int freq) {
 	
 }
 int getOPClassFromChannel(int channel){
-	if (channel == 36 |
-		channel == 40 |
-		channel == 44 |
-		channel == 48 ){
+	if (channel >= 36 ||
+		channel <= 48 ){
 
 		return 115; // 1 in nicht global
 	}
-	else if(channel == 52 |
-			channel == 56 |
-			channel == 60 |
-			channel == 64 ){
+	else if(channel >= 52 ||
+			channel <= 64 ){
 
 		return 118; // 2
 	}
 	else if(
-			channel == 100 |
-			channel == 104 |
-			channel == 108 |
-			channel == 112 |
-			channel == 116 |
-			channel == 120 |
-			channel == 124 |
-			channel == 128 |
-			channel == 132 |
-			channel == 136 |
-			channel == 140 ){
+			channel >= 100 ||
+			channel <= 140 ){
 
 		return 121; // 3
 	}
-	else if(channel == 1  |
-			channel == 2  |
-			channel == 3  |
-			channel == 4  |
-			channel == 5  |
-			channel == 6  |
-			channel == 7  |
-			channel == 8  |
-			channel == 9  |
-			channel == 10 |
-			channel == 11 |
-			channel == 12 |
-			channel == 13 ){
+	else if(channel >= 1  ||
+			channel <= 13 ){
 
 		return 81; // 4
 	}
